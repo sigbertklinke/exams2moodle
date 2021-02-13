fbf95450d0dd2d93c0f5789e62ffe55be82c7fb1
Sigbert Klinke <sigbert.klinke@web.de>
2021-02-13 20:27:00 +0100

    update exams2moodle_html

--- R/exams2moodle_html.R
+++ R/exams2moodle_html.R
@@ -6,29 +6,45 @@
 #' @param exam list: return list from \code{exam2moodle}
 #' @param name character: name of HTML file (default: \code{NULL})
 #' @param pattern character: string containing a regular expression to match list elements (default: \code{.})
+#' @param mathjax logical: should be MathJax loaded (default: \code{TRUE})
+#' @param header integer: at which level of the list a \code{<h2>...</h2>} element should be included (default: \code{2})
 #'
 #' @return invisibly the names of list elements in the HTML file
 #' @export
 #'
 #' @examples
 #' x <- 1
-exams2moodle_html <- function(exam, name=NULL, pattern=".") { 
+exams2moodle_html <- function(exam, name=NULL, pattern=".", mathjax=TRUE, header=2) { 
   lst  <- unlist(exam)
   nlst <- names(lst)
   elem <- grepl(pattern, nlst)
   stopifnot(sum(elem)>0)
   lst  <- lst[elem]
   nlst <- names(lst)
+  slst <- strsplit(nlst, '.', fixed=TRUE)
   if (!is.null(name)) {
-    ret  <- '<table>'
-    for (i in 1:length(lst)) {
-      ret <- c(ret, paste0('<tr><td bgcolor="grey" style="vertical-align: top;">',
-                           gsub('.', ' ', nlst[i], fixed = TRUE),
-                           '</td><td>',
-                           lst[i],
-                           '</td></tr>'))
+    ret  <- '<!DOCTYPE html><html><head>'
+    if (mathjax) {
+      ret  <- c(ret, '<script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>')
     }
-    ret <- c(ret, "</table>")
+    ret <- c(ret, '</head><body>')
+    if (header>0) lastheader <- rep('', header)
+    for (i in 1:length(lst)) {
+      if (header>0) {
+        if (any(lastheader!=slst[[i]][1:header])) {
+          if (i>1) ret <- c(ret, "</table>")
+          lastheader <- slst[[i]][1:header] 
+          ret <- c(ret, sprintf('<h2>%s</h2>', paste(lastheader, collapse=" ")))    
+          ret <- c(ret, '<table width="100%">')
+        }
+      }
+      ret <- c(ret, sprintf('<tr><td><div id="%s" width="100%%">%s</div></td>', nlst[i], lst[i]))
+      if (header>0) slst[[i]] <- slst[[i]][-(1:header)]
+      ret <- c(ret, sprintf('<td style="text-align:right;background-color: grey;">%s</td></tr>', 
+                            paste0(slst[[i]], collapse=" "))
+      )
+    }
+    ret <- c(ret, "</table></body></html>")
     if (all(!endsWith(name, c(".html", ".htm")))) name <- paste0(name, '_xml.html')
     writeLines(ret, name)
   }
